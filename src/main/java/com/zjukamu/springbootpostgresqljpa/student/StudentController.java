fa37493ae5b3b24fdde1292b4ee19bbfd0bc2c1e
Zjukamu Riki <zjukamu-riki@users.noreply.github.com>
2022-01-17 19:45:34 +0300

    add service layer

--- src/main/java/com/zjukamu/springbootpostgresqljpa/student/StudentController.java
+++ src/main/java/com/zjukamu/springbootpostgresqljpa/student/StudentController.java
@@ -4,26 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Bob",
-                        "bob@gmail.com",
-                        LocalDate.of(2000, Month.FEBRUARY, 6),
-                        21
-
-                )
-        );
+            return studentService.getStudents();
     }
 
 }
