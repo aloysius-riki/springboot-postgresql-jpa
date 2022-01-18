259385a2fb64574a71770e8e4301d765d3ab3cd0
Zjukamu Riki <zjukamu-riki@users.noreply.github.com>
2022-01-18 22:33:49 +0300

    Calculate age from dob instead of storing it in DB

--- src/main/java/com/zjukamu/springbootpostgresqljpa/student/StudentConfig.java
+++ src/main/java/com/zjukamu/springbootpostgresqljpa/student/StudentConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import static java.time.Month.*;
@@ -16,22 +15,20 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner (StudentRepository repository){
         return args -> {
-            Student bob = new Student(
-                    "Bob",
-                    "bob@gmail.com",
-                    LocalDate.of(2002, APRIL, 7),
-                    21
+            Student wanjiru = new Student(
+                    "Wanjiru",
+                    "wanjiru@gmail.com",
+                    LocalDate.of(2002, APRIL, 7)
             );
 
             Student kamau = new Student(
                     "Kamau",
                     "kamau@gmail.com",
-                    LocalDate.of(2008, APRIL, 7),
-                    21
+                    LocalDate.of(2008, APRIL, 7)
             );
 
             repository.saveAll(
-                    List.of(bob, kamau)
+                    List.of(wanjiru, kamau)
             );
         };
     }
